97f7b3fc7f5acf0330939d11e6db706ab862389a
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-07-24 03:53:03 +0900

    feature: implement get_cache_directory() and create_cache_directory() for the Unix.
        get_cache_directory(): get a cache directory per user.
        create_cache_directory(): create cache directory if it doesn't exist.

--- include/cache.h
+++ include/cache.h
@@ -5,4 +5,10 @@
 #ifndef GLSLC_P_CACHE_H
 #define GLSLC_P_CACHE_H
 
+#include <filesystem>
+
+std::filesystem::path get_cache_directory();
+
+void create_cache_directory(const std::filesystem::path& cache_directory);
+
 #endif //GLSLC_P_CACHE_H
